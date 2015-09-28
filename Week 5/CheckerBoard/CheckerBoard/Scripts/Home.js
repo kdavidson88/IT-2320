8c309a6c5326d054d9f219fe160fec4aab28c7c3
Kyle Davidson <kdavidson88@live.com>
2015-09-28 14:35:31 -0400

    Week 5

---
+++ Week 5/CheckerBoard/CheckerBoard/Scripts/Home.js
@@ -0,0 +1,42 @@
+﻿var Home = {}
+
+Home.HighlightBackground = function ()
+{
+    $(".cell").click(function()
+    {
+        var cell = $(this);
+        if (cell)
+        {
+            $(this).toggleClass("highlighted");
+        }
+        else
+        {
+            $(this).toggleClass("cell");
+        }
+        
+    })
+
+}
+
+
+
+
+$(document).ready(function ()
+{
+    var cells = $(".cell");
+    var colorCount = 0;
+
+    for (var i = 0; i < cells.length; i++)
+    {
+        var cell = $(cells[i]);
+        var isDark = colorCount % 2 == 0;
+        var isNextRow = (i + 1) % 8 == 0;
+        colorCount += isNextRow ? 2 : 1;
+        cell.css("background-color", isDark ? "navy" : "white");
+    }   
+    
+    Home.HighlightBackground();
+
+    
+});
+
