aad5eb6885b7e0c37b09d4e1d2ce157b989bfcae
Kyle Davidson <kdavidson88@live.com>
2015-10-06 23:30:34 -0400

    Week 7

---
+++ Week 7/scripts.js
@@ -0,0 +1,58 @@
+var games = [
+	{
+	 "title" : "Mass Effect",
+	 "genre" : "Third-Person Shooter",
+	 "date" : "March 6, 2012",
+	 "character" : "Shepard",
+	 "weapon" [
+	   {
+		"Pistol" : "Kessler",
+		"Assault Rifle" : "Lancer",
+		"Shotgun" : "Sokolov",
+		"Sniper Rifle" : "Titan"
+	   }
+	 ]
+	{
+	 "title" : "Resident Evil",
+	 "genre" : "Survival Horror",
+	 "date" : "March 22, 1996"
+	 "character" : "Chris Redfield"
+	 "weapon" [
+	   {
+		"Handgun" : "Nine-Oh-Nine",
+		"Assault Rifle" : "Bear Commander",
+		"Shotgun" : "Triple Shot",
+		"Rifle" : "Anti-Matter Rifle"
+	   }
+	 ] 
+	}
+	{
+	 "title" : "Halo",
+	 "genre" : "First-Person Shooter",
+	 "date" : "November 15, 2001",
+	 "character" : " Master Chief",
+	 "weapons" [
+	   {
+	    "Pistol" : "M6D Personal Defense Weapon",
+		"Assault Rifle" : "MA5B Individual Combat Weapon",
+		"Shotgun" : "Oathsworn",
+		"Rifle" : "BR85 Heavy Barrel Service Rifle"
+	   }
+	 ]
+	}
+	{
+	 "title" : "Final Fantasy 7",
+	 "genre" : "Role-Playing Game",
+	 "date" : "January 31, 1997",
+	 "character" : "Cloud Strife",
+	 "weapon" 	[
+	   {
+		"Two-Handed Sword" : "Buster Sword",
+		"Katana" : "Murasame",
+		"Blunt Weapon" : "Nail Bar",
+		"Ultimate Weapon" : "Ultima Weapon"
+	   }
+	]
+	}
+	}
+]
