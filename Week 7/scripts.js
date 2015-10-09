3f74a8bc5321bc57dd76bc6f6a773af8b88c1d05
Kyle Davidson <kdavidson88@live.com>
2015-10-09 18:47:24 +0800

    Week 7

--- Week 7/scripts.js
+++ Week 7/scripts.js
@@ -4,55 +4,62 @@ var games = [
 	 "genre" : "Third-Person Shooter",
 	 "date" : "March 6, 2012",
 	 "character" : "Shepard",
-	 "weapon" [
+	 "weapon" : [
 	   {
 		"Pistol" : "Kessler",
 		"Assault Rifle" : "Lancer",
 		"Shotgun" : "Sokolov",
 		"Sniper Rifle" : "Titan"
 	   }
-	 ]
-	{
+	 ],
+	
 	 "title" : "Resident Evil",
 	 "genre" : "Survival Horror",
-	 "date" : "March 22, 1996"
-	 "character" : "Chris Redfield"
-	 "weapon" [
+	 "date" : "March 22, 1996",
+	 "character" : "Chris Redfield",
+	 "weapon" : [
 	   {
 		"Handgun" : "Nine-Oh-Nine",
 		"Assault Rifle" : "Bear Commander",
 		"Shotgun" : "Triple Shot",
 		"Rifle" : "Anti-Matter Rifle"
 	   }
-	 ] 
-	}
-	{
+	 ], 
+	
+	
 	 "title" : "Halo",
 	 "genre" : "First-Person Shooter",
 	 "date" : "November 15, 2001",
 	 "character" : " Master Chief",
-	 "weapons" [
+	 "weapon" : [
 	   {
 	    "Pistol" : "M6D Personal Defense Weapon",
 		"Assault Rifle" : "MA5B Individual Combat Weapon",
 		"Shotgun" : "Oathsworn",
 		"Rifle" : "BR85 Heavy Barrel Service Rifle"
 	   }
-	 ]
-	}
-	{
+	 ],
+
+	
 	 "title" : "Final Fantasy 7",
 	 "genre" : "Role-Playing Game",
 	 "date" : "January 31, 1997",
 	 "character" : "Cloud Strife",
-	 "weapon" 	[
+	 "weapon" :	[
 	   {
 		"Two-Handed Sword" : "Buster Sword",
 		"Katana" : "Murasame",
 		"Blunt Weapon" : "Nail Bar",
 		"Ultimate Weapon" : "Ultima Weapon"
 	   }
-	]
-	}
+	],
 	}
 ]
+
+
+$(document).ready(function()
+{
+	obj = JSON.parse(games);
+	document.body.innerHTML = games[0].title;
+	
+});
