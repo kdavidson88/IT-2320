c28e77a5e77ffed85f425fbfdcaaa8e4f40b98e1
Kyle Davidson <kdavidson88@live.com>
2015-10-12 15:52:07 -0400

    Week 7

--- Week 7/scripts.js
+++ Week 7/scripts.js
@@ -1,65 +1,72 @@
-var games = [
+var vidGames =
+{
+"games" : [
 	{
-	 "title" : "Mass Effect",
-	 "genre" : "Third-Person Shooter",
-	 "date" : "March 6, 2012",
-	 "character" : "Shepard",
-	 "weapon" : [
-	   {
-		"Pistol" : "Kessler",
-		"Assault Rifle" : "Lancer",
-		"Shotgun" : "Sokolov",
-		"Sniper Rifle" : "Titan"
-	   }
-	 ],
-	
-	 "title" : "Resident Evil",
-	 "genre" : "Survival Horror",
-	 "date" : "March 22, 1996",
-	 "character" : "Chris Redfield",
-	 "weapon" : [
-	   {
-		"Handgun" : "Nine-Oh-Nine",
-		"Assault Rifle" : "Bear Commander",
-		"Shotgun" : "Triple Shot",
-		"Rifle" : "Anti-Matter Rifle"
-	   }
-	 ], 
-	
-	
-	 "title" : "Halo",
-	 "genre" : "First-Person Shooter",
-	 "date" : "November 15, 2001",
-	 "character" : " Master Chief",
-	 "weapon" : [
-	   {
-	    "Pistol" : "M6D Personal Defense Weapon",
-		"Assault Rifle" : "MA5B Individual Combat Weapon",
-		"Shotgun" : "Oathsworn",
-		"Rifle" : "BR85 Heavy Barrel Service Rifle"
-	   }
-	 ],
-
-	
-	 "title" : "Final Fantasy 7",
-	 "genre" : "Role-Playing Game",
-	 "date" : "January 31, 1997",
-	 "character" : "Cloud Strife",
-	 "weapon" :	[
-	   {
-		"Two-Handed Sword" : "Buster Sword",
-		"Katana" : "Murasame",
-		"Blunt Weapon" : "Nail Bar",
-		"Ultimate Weapon" : "Ultima Weapon"
-	   }
-	],
+		"title" : "Mass Effect",
+		"genre" : "Third-Person Shooter",
+		"date" : "March 6, 2012",
+		"character" : "Shepard",
+		"weapon" : [
+			{
+			 "Primary" : "Kessler",
+			 "Secondary" : "Lancer",
+			}
+		]	
+	},
+	{
+		"title" : "Resident Evil",
+		"genre" : "Survival Horror",
+		"date" : "March 22, 1996",
+		"character" : "Chris Redfield",
+		"weapon" : [
+			{
+			 "Primary" : "Nine-Oh-Nine",
+			 "Secondary" : "Bear Commander",
+			}
+		]
+	},
+	{
+		"title" : "Halo",
+		"genre" : "First-Person Shooter",
+		"date" : "November 15, 2001",
+		"character" : " Master Chief",
+		"weapon" : [
+			{
+			 "Primary" : "M6D Personal Defense Weapon",
+			 "Secondary" : "MA5B Individual Combat Weapon",
+			}
+		]
+	},
+	{
+		"title" : "Final Fantasy 7",
+		"genre" : "Role-Playing Game",
+		"date" : "January 31, 1997",
+		"character" : "Cloud Strife",
+		"weapon" :	[
+			{
+			 "Primary" : "Buster Sword",
+			 "Secondary" : "Ultima Weapon",
+			}
+		]
 	}
 ]
-
+}
 
 $(document).ready(function()
 {
-	obj = JSON.parse(games);
-	document.body.innerHTML = games[0].title;
-	
-});
+	for(i = 0; i < vidGames.games.length; i++)
+	{
+		var game = vidGames.games[i];
+		for(j = 0; j < game.weapon.length; j++)
+		{
+			var weap = game.weapon[j];
+			document.getElementById("showMeTheJSON").innerHTML += "<table>" + "<tr><td>" +
+				game.title + "    </td><td>" +
+				game.genre + "</td><td>" + 
+				game.date +"</td><td>" + 
+				game.character + "</td></tr>" + "<tr><td>" + "<em>" + weap.Primary +
+					"</td><td>" + "<em>" + weap.Secondary + "</td></tr>" + "<hr>"
+		}			
+			
+	}
+})
